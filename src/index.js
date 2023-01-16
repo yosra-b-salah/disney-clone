c13bb90429eeea0ee851fd0c60a36161af864eab
YosraB <bensalah.yosra@hotmail.com>
2023-01-17 00:43:19 +0100

    fix error redux provider

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import store from './app/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<Provider state={store}>
+		<Provider store={store}>
 			<App />
 		</Provider>
 	</React.StrictMode>
