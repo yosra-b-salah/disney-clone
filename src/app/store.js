092e31c9bd463af36a17821131110515e57c432b
YosraB <bensalah.yosra@hotmail.com>
2023-01-17 00:08:21 +0100

    redux store + userSlice reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import userReducer from "../features/users/userSlice";
+
 export default configureStore({
-	reducer: {},
+	reducer: {
+		user: userReducer,
+	},
 
 });
+
 
