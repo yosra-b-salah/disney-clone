7915a7d09dcf586d86a24873f1cd03780098eaf1
YosraB <bensalah.yosra@hotmail.com>
2023-01-17 00:47:06 +0100

    fix import userSlice path in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import userReducer from "../features/users/userSlice";
+import userReducer from "../features/user/userSlice";
 
 export default configureStore({
 	reducer: {
