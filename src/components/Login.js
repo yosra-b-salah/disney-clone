72f5ee5f186b4d07776aad1f394e871cd207cdda
YosraB <bensalah.yosra@hotmail.com>
2023-01-16 11:16:09 +0100

    header component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+
 import bckgImg from '../images/login-background.jpg';
 import logo1 from '../images/cta-logo-one.svg';
 import logo2 from '../images/cta-logo-two.png';
+
 const Login = (props) => {
 	return (<Container>
 		<Content>
@@ -19,13 +21,14 @@ const Login = (props) => {
 		</Content>
 	</Container>)
 }
+export default Login;
+
 const Container = styled.section`
 overflow: hidden;
 display: flex;
 flex-direction: column;
 text-align: center;
-height: 100vh;
-`;
+height: 100vh;`;
 
 const Content = styled.div`
 margin-bottom: 10vw;
@@ -38,8 +41,7 @@ justify-content: center;
 align-items: center;
 flex-direction: column;
 padding: 80px 40px;
-height: 100%;
-`;
+height: 100%;`;
 
 const BackgroundImage = styled.div`
 height: 100%;
@@ -51,8 +53,8 @@ position: absolute;
 top: 0;
 right: 0;
 left: 0;
-z-index: -1;
-`;
+z-index: -1;`;
+
 const CTA = styled.div`
 margin-bottom: 2vw;
 max-width: 650px;
@@ -66,16 +68,15 @@ margin-right: auto;
 margin-left: auto;
 transition-timing-function: ease-out;
 transition: opacity 0.2s;
-width: 100%;
+width: 100%;`;
 
-`;
 const CTALogoOne = styled.img`
 margin-bottom: 12px;
 max-width: 600px;
 min-height: 1px;
 display: block;
-width: 100%;
-`;
+width: 100%;`;
+
 const SignUp = styled.a`
 font-weight: bold;
 color: #f9f9f9;
@@ -87,24 +88,21 @@ font-size: 18px;
 padding: 16.5px 0;
 border: 1px solid transparent;
 border-radius: 4px;
-&:hover {
-	background-color: #0483ee;
-}
+	&:hover {
+		background-color: #0483ee;
+	}
 `;
+
 const Desription = styled.h2`
 color: hsla(0, 0%, 95.3%, 1);
 font-size: 11px;
 margin: 0 0 24px;
 line-height: 1.5;
-letter-spacing: 1.5px
-`;
+letter-spacing: 1.5px;`;
 
 const CTALogoTwo = styled.img`
 max-width: 600px;
 margin-bottom: 20px;
 display: inline-block;
 vertical-align: bottom;
-width: 100%;
-`;
-
-export default Login
+width: 100%;`;
