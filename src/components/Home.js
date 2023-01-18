bc77ee862968dfc7d7544d67a65aac91a4903ea8
YosraB <bensalah.yosra@hotmail.com>
2023-01-18 19:09:01 +0100

    slider added to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,21 +10,22 @@ const Home = () => {
 	)
 }
 
-const Container = styled.div`
-	position: relative;
-	min-height: calc(100vh - 250px);
-	overflow-x: hidden;
-	display: block; 
-	top: 72px;
-	padding: 0 calc(3.5vw + 5px);
-	&:after {
-		content: "";
-		background: url(${bkgImage}) center center / cover
-			no-repeat fixed;
-		position: absolute;
-		inset: 0px;
-		opacity: 1;
-		z-index: -1;
-	}
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url(${bkgImage}) center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
 `;
 export default Home
--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styled from "styled-components";
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from 'react-slick';
+import Slider from "react-slick";
+
+
+import slideImg1 from "../images/slider-badag.jpg";
+import slideImg2 from "../images/slider-badging.jpg";
+import slideImg3 from "../images/slider-scale.jpg";
+import slideImg4 from "../images/slider-scales.jpg";
 
 const ImgSlider = (props) => {
 	let settings = {
@@ -12,23 +17,101 @@ const ImgSlider = (props) => {
 		speed: 500,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		autoPlay: true,
-	}
+		autoplay: true,
+	};
 	return (
 		<div>
-			<Slider {...settings} >
-				<div>
-					<h3>Slider 1</h3>
-				</div>
-				<div>
-					<h3>Slider 2</h3>
-				</div>
-				<div>
-					<h3>Slider 3</h3>
-				</div>
-			</Slider>
+			<Carousel {...settings} >
+				<Wrap>
+					<a>
+						<img src={slideImg1} />
+					</a>
+				</Wrap>
+				<Wrap>
+					<a>
+						<img src={slideImg2} />
+					</a>
+				</Wrap>
+				<Wrap>
+					<a>
+						<img src={slideImg3} />
+					</a>
+				</Wrap>
+				<Wrap>
+					<a>
+						<img src={slideImg4} />
+					</a>
+				</Wrap>
+
+			</Carousel>
 		</div>
 	)
 }
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: initial;
+  }
+
+  .slick-prev {
+    left: -75px;
+  }
+
+  .slick-next {
+    right: -75px;
+  }
+`;
+
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+
+  a {
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
+    &:hover {
+      padding: 0;
+      border: 4px solid rgba(249, 249, 249, 0.8);
+      transition-duration: 300ms;
+    }
+  }
+`;
 
 export default ImgSlider
