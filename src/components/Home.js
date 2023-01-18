9181a01e6d432960b3d20d57a9ceb310c6cc6e36
YosraB <bensalah.yosra@hotmail.com>
2023-01-18 21:22:20 +0100

    new + trending+originals show

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,19 @@ import bkgImage from "../images/home-background.png";
 import ImgSlider from "./ImgSlider";
 import Viewers from './Viewers';
 import Recommends from './Recommends';
+import NewDisney from './NewDisney';
+import Originals from './Originals';
+import Trending from './Trending';
+
 const Home = () => {
 	return (
 		<Container>
 			<ImgSlider />
 			<Viewers />
 			<Recommends />
+			<NewDisney />
+			<Originals />
+			<Trending />
 		</Container>
 	)
 }
